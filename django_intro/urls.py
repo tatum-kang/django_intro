6a1bea045a80157a805409f82c1f04a26c1f2698
강희원 <heewkk@gmail.com>
2019-08-14 16:26:20 +0900

    Learn variable routing

--- django_intro/urls.py
+++ django_intro/urls.py
@@ -21,7 +21,10 @@ from pages import views
 # 정의x 경로 라면 404 not found
 urlpatterns = [
     #path('사용자가 접속하는 경로', )
-    path('dinner/', views.dinner),
+    path('times/<int:num1>/<int:num2>/', views.times),
+    path('greeting/<str:name>/', views.greeting),
+    path('image/', views.image),
+    path('dinner/<str:name>/', views.dinner),
     path('introduce/', views.introduce),
     path('index/', views.index),
     path('admin/', admin.site.urls),
--- pages/views.py
+++ pages/views.py
@@ -13,12 +13,36 @@ def introduce(request):
 
 
 # template variable example
-def dinner(request):
+def dinner(request, name):
     menu = ['강남 더막창스', '노랑통닭', '양자강']
     pick = random.choice(menu)
     context = {
         'pick': pick,
+        'name': name,
     }
     # django template 으로 context 전달
     return render(request, 'dinner.html', context)
-    
+
+
+def image(request):
+    img = 'https://picsum.photos/500'
+    context = {
+        'img': img,
+    }
+    return render(request, 'image.html', context)
+
+
+def greeting(request, name):
+    context = {
+        'name': name
+    }
+    return render(request, 'greeting.html', context)
+
+
+def times(request, num1, num2):
+    context = {
+        'result': num1 * num2,
+        'num1': num1,
+        'num2': num2,
+    }
+    return render(request, 'times.html', context)
